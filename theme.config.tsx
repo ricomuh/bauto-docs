5a4cdee3082acc9043d0db572b6b8d377727e7c5
ricomuh <rico.smartlolo@gmail.com>
2023-05-11 00:24:09 +0700

    routing

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Bauto</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/ricomuh/bauto-docs",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/ricomuh/bauto-docs",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Copyright Rico Muhammad",
   },
-}
+};
 
-export default config
+export default config;
